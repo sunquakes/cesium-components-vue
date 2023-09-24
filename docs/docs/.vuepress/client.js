7189447b939c17e3e3e9b60d53468f4f0ba83cfe
Shing <sunquakes@outlook.com>
2023-09-24 20:50:38 +0800

    Fixed can not find cesium files.

--- docs/docs/.vuepress/client.js
+++ docs/docs/.vuepress/client.js
@@ -4,7 +4,7 @@ import CesiumComponentsVue from 'cesium-components-vue'
 if (process.env.NODE_ENV === 'development') {
   if (typeof window !== 'undefined') window.CESIUM_BASE_URL = '/Cesium'
 } else {
-  if (typeof window !== 'undefined') window.CESIUM_BASE_URL = '/ajax/libs/cesium/1.109.0'
+  if (typeof window !== 'undefined') window.CESIUM_BASE_URL = 'https://cdnjs.cloudflare.com/ajax/libs/cesium/1.109.0'
 }
 
 export default defineClientConfig({
