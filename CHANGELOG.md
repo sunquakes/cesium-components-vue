c399466417fdbabffb3a38a056fab2f791bc1c92
Shing <sunquakes@outlook.com>
2023-09-24 20:35:11 +0800

    CHANGELOG and fixed can not find cesium files.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## [0.2.0] - 2023-09-24
+
+### Added
+- Added Amap Viewer component.
+- Added viewer custom color.
+- Added ellipse fade material.
+---
+
 ## [0.0.1] - 2023-09-16
 
 ### Added
--- docs/docs/.vuepress/client.js
+++ docs/docs/.vuepress/client.js
@@ -1,5 +1,11 @@
 import { defineClientConfig } from '@vuepress/client'
-import CesiumComponentsVue from '../../../src/index'
+import CesiumComponentsVue from 'cesium-components-vue'
+
+if (process.env.NODE_ENV === 'development') {
+  if (typeof window !== 'undefined') window.CESIUM_BASE_URL = '/Cesium'
+} else {
+  if (typeof window !== 'undefined') window.CESIUM_BASE_URL = '/ajax/libs/cesium/1.109.0'
+}
 
 export default defineClientConfig({
   enhance({ app }) {
